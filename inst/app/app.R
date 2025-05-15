96877b4326cd84cd4ebd4bb93c8f45a269a92431
Lucas-Straub <lucas.straub@student.uva.nl>
2025-05-15 12:08:33 +0200

    added check_csv function and documentation

--- inst/app/app.R
+++ inst/app/app.R
@@ -18,62 +18,45 @@ ui <- fluidPage(
 )
 
 
-# ============ Server ========================================================
+# ============ Server ==========================================================
 server <- function(input, output, session) {
+
+
+#--------- check csv and return output------------------------------------------
 
   # This will store messages to show to the user
   fileStatus <- reactiveVal(NULL)
 
-  data <- reactive({ # reactive means the servers waits for something to happen
+  # check_csv() function to validate the file
+  data <- reactive({ # reactive means the server waits for something to happen
     req(input$file)  # Don’t try this unless a file has actually been uploaded
-      filename <- input$file$name
-
-      # Check file extension
-      if (!grepl("\\.csv$", filename, ignore.case = TRUE)) {
-        fileStatus("❌ Error: The uploaded file is not a CSV.")
-        return(NULL)
-      }
-      # Try reading file
-      df <- tryCatch(
-        read.csv(input$file$datapath),  # gives the temporary location of the uploaded
-                                        #file: data() is now a shortcut to the uploaded file
-        error = function(e) {
-          fileStatus("❌ Error: Failed to read the CSV file.")
-          return(NULL)
-        }
-      )
-
-
-      fileStatus(NULL)  # Clear message if all good
-      return(df)
+    result <- check_csv(input$file$datapath)
+    fileStatus(result$message)
+    return(result$data)
   })
 
-
-  # Only show result message if CSV is valid
-  output$checkResult <- renderText({# output is a list of outputs the app will show
-                                      # with  $preview im telling the server where to
-                                      # display it in the ui at the location where
-                                      # Table output "preview".
-                                      # render_something tells ui what form the output
-                                      # should have
+  # Show the result message from check_csv()
+  output$checkResult <- renderText({
     req(input$check)
     isolate({
       df <- data()
-
-      # If data is NULL (bad format or load failed), show stored message
-      if (is.null(df)) {
-        return(fileStatus())
-      }
-
-      # Else check for missing values
-      if (any(is.na(df))) {
-        return("⚠️ Data contains missing values (NA). Please clean it before proceeding.")
-      } else {
-        return("✅ Data looks good. No missing values detected.")
-      }
+      return(fileStatus())
     })
   })
-}
 
+
+#--------- Compute Networks ------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+}
 
 shinyApp(ui, server)
